25acd76fcbd4aceff35648e240850f5ae54f02ca
MatheusPQ <matheuspq79@outlook.com.br>
2018-12-23 21:21:48 -0200

    Erro aula 19

--- wp-content/plugins/filmes-reviews/filmes-reviews.php
+++ wp-content/plugins/filmes-reviews/filmes-reviews.php
@@ -11,11 +11,13 @@ Text Domain: filmes-reviews
 License: GPL2
 */
 
-require dirname(__FILE__).'lib/class-tgm-plugin-activation.php';
+require dirname(__FILE__).'\lib\class-tgm-plugin-activation.php';
+define('TEXT_DOMAIN', 'filmes-reviews');
 
 //Padrão Singleton
 class Filmes_reviews {
     private static $instance;
+    // const TEXT_DOMAIN = "filmes-reviews";
 
     public static function getInstance(){
         if(self::$instance == NULL){
@@ -27,6 +29,7 @@ class Filmes_reviews {
         //Não precisa usar o $this, pois $instance é 'static'.
         // add_action('init', [$this, 'register_post_type']);
         add_action('init', 'Filmes_reviews::register_post_type');
+        add_action('tgmpa_register', [$this, 'check_required_plugins']);
     }
 
     public static function register_post_type(){
@@ -58,6 +61,31 @@ class Filmes_reviews {
                 'force_desactivation' => false
             ],
         ];
+
+        // Config
+
+        $config = array(
+            'domain' => TEXT_DOMAIN,
+            'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+            'default_path' => '',                      // Default absolute path to bundled plugins.
+            'menu'         => 'install-required-plugins', // Menu slug.
+            'parent_slug'  => 'plugins.php',            // Parent menu slug.
+            'capability'   => 'update_plugins',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+            'has_notices'  => true,                    // Show admin notices or not.
+            'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+            'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+            'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+            'message'      => '',                      // Message to output right before the plugins table.
+            
+            'strings'      => array(
+                'page_title'                      => __( 'Install Required Plugins', TEXT_DOMAIN ),
+                'menu_title'                      => __( 'Install Plugins', TEXT_DOMAIN ),
+                // <snip>...</snip>
+                'nag_type'                        => 'updated', // Determines admin notice type - can only be 'updated', 'update-nag' or 'error'.
+            )
+            
+        );
+        // Fim Config
     }
 
     //Quando o plugin for instalado, a regra de rewrite será feita automaticamente (ao invés de ter que ir manualmente em 'Configurações' e apenas clicar em 'Salvar Alterações').
